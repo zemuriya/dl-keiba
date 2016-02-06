b96de524f6a20db43eddda8af930e775153649e7
kento-tajiri <k.denjiry@gmail.com>
2016-02-07 08:42:07 +0900

    add chainer tutorial import

--- test_deap_keiba.py
+++ test_deap_keiba.py
@@ -1,7 +1,15 @@
+# -*- coding: utf-8 -*-
+##上記がないと日本語のコメントがエラーになる http://linux.oboe-gaki.com/archives/000379.html
+
 import numpy as np
+import chainer
+from chainer import cuda, Function, gradient_check, Variable, optimizers, serializers, utils
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+
 import scipy as sp
 import matplotlib.pyplot as plt
 import sklearn
-import chainer
 from sklearn import svm
 from sklearn import cross_validation
